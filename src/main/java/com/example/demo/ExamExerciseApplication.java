d382f08278e3e4baa1d753dbdd2e169560c6a0e3
adolfoalejandroar <adolfoalejandroar@ufps.edu.co>
2025-04-28 10:47:18 -0500

    Prueba

---
+++ src/main/java/com/example/demo/ExamExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamExerciseApplication.class, args);
+	}
+
+}
